20428246b9d052b08c4412505db94b21a23a7fb6
PaddyItap <patrick.finger@iem.thm.de>
2021-12-07 17:02:19 +0100

    RecordingFragment.java angepasst: - die jeweilige verstrichene Zeit wird zusätzlich noch berechnet - also: 1. Mittelwerte - 3s ; 2. Mittelwerte - 6s ; ...

--- app/src/main/java/mow/thm/de/sleeptracker3/RecordingFragment.java
+++ app/src/main/java/mow/thm/de/sleeptracker3/RecordingFragment.java
@@ -59,6 +59,9 @@ public class RecordingFragment extends Fragment implements SensorEventListener {
     private Sensor mySensor;
     private SensorManager SM;
     Timer timer;
+    long start;
+    long end;
+    long delta;
     ArrayList<Float> movementDataX;
     ArrayList<Float> movementDataY;
     ArrayList<Float> movementDataZ;
@@ -164,6 +167,8 @@ public class RecordingFragment extends Fragment implements SensorEventListener {
             @Override
             public void onClick(View view) {
 
+                start = System.currentTimeMillis();
+
                 movementDataX.clear();
                 movementDataY.clear();
                 movementDataZ.clear();
@@ -174,9 +179,15 @@ public class RecordingFragment extends Fragment implements SensorEventListener {
                 startSensorBtn.setEnabled(false);
                 createSensor();
 
+
                 timer.schedule(new TimerTask() {
                     @Override
                     public void run() {
+                        System.out.println("TEEEEEESSSSST");
+
+                        end = System.currentTimeMillis();
+                        delta = end - start;
+
                         float x = 0;
                         float y = 0;
                         float z = 0;
@@ -207,6 +218,7 @@ public class RecordingFragment extends Fragment implements SensorEventListener {
                         System.out.println("Im Mittel alle 3 Sekunden auf der X Achse: " + x);
                         System.out.println("Im Mittel alle 3 Sekunden auf der Y Achse: " + y);
                         System.out.println("Im Mittel alle 3 Sekunden auf der Z Achse: " + z);
+                        System.out.println("Abgelaufene Zeit in ms: " + delta/1000);
 
                         movementDataX.clear();
                         movementDataY.clear();
